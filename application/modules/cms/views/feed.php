cdb4dc9a36345f65cb8e0b0c5dde83b3eef018ee
sushrut kasture <sushrut kasture>
2016-08-27 17:56:41 +0530

    bootstrap

--- application/modules/cms/views/feed.php
+++ application/modules/cms/views/feed.php
@@ -1,3 +1,4 @@
+<div class="jumbotron">
 <div class="form-group row">
 	<div class="col-md-4">
 		<select class="selectpicker" id="ThreadTopic">
@@ -7,15 +8,18 @@
   			<option value="3">LIBRARY</option>
   			<option value="4">OTHER</option>
 		</select>
+		<br>
+		<input type="" class="input-group form-control" id="ThreadName">
 	</div>
-	<div class="col-md-8">
-		<textarea class="form-control" rows="5" id="ThreadText"></textarea>		
+	<div class="col-md-8 ">
+		<textarea class="input-group input-group-lg form-control" rows="10" id="ThreadText"></textarea>		
 	</div>
 </div>
 
-<div class="card card-block">
+<div class="card card-block col-md-3">
   <h4 class="card-title">Card title</h4>
   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   <a href="#" class="card-link">Card link</a>
   <a href="#" class="card-link">Another link</a>
+</div>
 </div>
